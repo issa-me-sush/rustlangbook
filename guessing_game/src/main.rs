5b3b9a5ff76f47fee82b2345d962ac70143f185d
sushthecoda <coolsushaan@gmail.com>
2023-05-19 00:02:31 +0530

    guessing game done

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,32 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+use colored::*;
 fn main() {
-    println!("Hello, world!");
+    
+    let randomnum = rand::thread_rng().gen_range(2, 53);
+    loop{
+        println!("guess the number!");
+       
+        println!("please enter the number you guessed: ");
+    
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("failed to read line");
+        // let guess:u32 = guess.trim().parse().expect("shuda entered a number!");
+        let guess:u32 = match  guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+
+        };
+        println!("you guessed {}", guess); 
+    
+        match guess.cmp(&randomnum){
+           Ordering::Equal =>{println!("{}","yay! you are right!".green());
+                              break;} ,
+           Ordering::Less => println!("{}","its something larger than that!".red()),
+           Ordering::Greater => println!("{}","its something smaller than that!".red()),
+        }
+        println!("the correct number is: {}",randomnum);
+    }
+
 }
